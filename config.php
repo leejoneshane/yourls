258e88fa97d73c5af922286a999887ef25b9705d
Sean Lee <leejoneshane@gmail.com>
2021-02-23 12:19:56 +0800

    Update config.php

--- config.php
+++ config.php
@@ -37,7 +37,7 @@ define( 'YOURLS_DB_PREFIX', 'yourls_' );
  ** All lowercase, no trailing slash at the end.
  ** If you define it to "http://sho.rt", don't use "http://www.sho.rt" in your browser (and vice-versa)
  ** To use an IDN domain (eg http://héhé.com), write its ascii form here (eg http://xn--hh-bjab.com) */
-define( 'YOURLS_SITE', env('URL') );
+define( 'YOURLS_SITE', env('DOMAIN') );
 
 /** YOURLS language
  ** Change this setting to use a translation file for your language, instead of the default English.
